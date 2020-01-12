84e1ca7e241bb0f782aac98e38b2ed5a33dc5bb7
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 17:31:09 -0500

    Add the authorization handler.

---
+++ src/Authorization/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ezoheux\App\Authorization;
+
+use App\Role;
+use App\User;
+
+/**
+ * The authorization handler.
+ */
+class Handler
+{
+    /**
+     * Adds the authorization gates for the role functionality.
+     *
+     * @return void Returns nothing.
+     */
+    public static function AddRoleGates()
+    {
+        $roles = Role::all();
+        foreach ($roles as $role) {
+            Gate::define("role-{$role->slug}", function (User $user) {
+                if ($user->hasRole($role->slug)) {
+                    return true;
+                }
+                return false;
+            });
+        }
+    }
+}
