b54f504f0ca6b89a0190a17b53e5681e0f2e47e3
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 17:13:47 -0500

    Add the service provider.

---
+++ src/AutopulseServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ezoheux\App;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * The autopulse service provider.
+ */
+class AutopulseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void Returns nothing.
+     */
+    public function boot()
+    {
+    }
+}
