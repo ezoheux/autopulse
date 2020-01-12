2ac974c875a71a808dd9375b5b4f28dc0b2b9f3d
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 17:01:57 -0500

    Add the role functionality trait.

---
+++ src/Traits/ImplementsRoleFunctionality.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ezoheux\App\Traits;
+
+/**
+ * The implements role functionality trait.
+ */
+trait ImplementsRoleFunctionality
+{
+    /**
+     * The relationship between the role model.
+     *
+     * @return mixed Returns the relationship.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role');
+    }
+
+    /**
+     * Check to see if the user has a role.
+     *
+     * @param mixed $role The role to check against.
+     *
+     * @return bool Returns true if the user has role and false if not.
+     */
+    public function hasRole($role)
+    {
+        if (is_array($role)) {
+            return null !== $this->roles()->whereIn('name', $role)->first();
+        }
+        return null !== $this->roles()->where('name', $role)->first();
+    }
+}
