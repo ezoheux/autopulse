a254aff09185237ac5329be5d02468d49d02666e
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 22:40:52 -0500

    Add implements permission functionality trait.

--- src/Traits/ImplementsPermissionFunctionality.php
+++ src/Traits/ImplementsPermissionFunctionality.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace Ezoheux\App\Traits;
+
+/**
+ * The implements permission functionality trait.
+ */
+trait ImplementsPermissionFunctionality
+{
+    /**
+     * Check to see if the user has a permission.
+     *
+     * @param mixed $permission The permission to check against.
+     *
+     * @return bool Returns true if the user has permission and false if not.
+     */
+    public function hasPermission($permission)
+    {
+        if (is_array($role)) {
+            return null !== $this->roles()->permissions()->whereIn('name', $permission)->first();
+        }
+        return null !== $this->roles()->permissions()->where('name', $permission)->first();
+    }
+}
