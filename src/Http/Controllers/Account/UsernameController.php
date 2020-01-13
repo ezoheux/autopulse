730bc661f9da0f9a1b46345f902d04991f7316dd
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 22:19:53 -0500

    Add username controller logic.

--- src/Http/Controllers/Account/UsernameController.php
+++ src/Http/Controllers/Account/UsernameController.php
@@ -1,1 +1,30 @@
+<?php
+
+namespace Ezoheux\App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+use Ezoheux\App\Traits\UpdatesUsernames;
+use Illuminate\Http\Request;
+
+/**
+ * The username controller.
+ */
+class UsernameController extends Controller
+{
+    use UpdatesUsernames;
+
+    /** @var string $redirectTo Where to redirect users after the password has been updated. */
+    protected $redirectTo = config('autopulse.username_handler_redirect_to', '/account/username');
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('verified');
+    }
+}
 
