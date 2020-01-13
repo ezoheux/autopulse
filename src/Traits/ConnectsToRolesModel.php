0e7043e07b68cf20a5e655b07f070b45a6c911bf
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-12 22:29:44 -0500

    Add trait logic.

--- src/Traits/ConnectsToRolesModel.php
+++ src/Traits/ConnectsToRolesModel.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace Ezoheux\App\Traits;
+
+/**
+ * The connects to roles model trait.
+ */
+trait ConnectsToRolesModel
+{
+    /** @var array $fillable The attributes that are mass assignable. */
+    protected $fillable = [
+        'slug', 'display', 'description',
+    ];
+
+    /**
+     * The relationship between the role model.
+     *
+     * @return mixed Returns the relationship.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role');
+    }
+}
