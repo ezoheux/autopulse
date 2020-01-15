e4a9adfdd5b983499dfa7ff15f5199d2e5be339b
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:40:17 -0500

    Improve route structure.

--- src/Autopulse.php
+++ src/Autopulse.php
@@ -14,11 +14,23 @@ class Autopulse
      */
     public static function routes()
     {
-        Route::get('/account/password', 'Autopulse\PasswordController@show')->name('password.handler');
-        Route::post('/account/password', 'Autopulse\PasswordController@update')->name('password.handler.update');
-        if (config('autopulse.username_handler', false)) {
-            Route::get('/account/username', 'Autopulse\UsernameController@show')->name('username.handler');
-            Route::post('/account/username', 'Autopulse\UsernameController@update')->name('username.handler.update');
+        Route::prefix('/account')->middleware(['password.confirm'])->group(function () {
+            Route::get('/account/password', 'Autopulse\PasswordController@show')->name('password.handler');
+            Route::post('/account/password', 'Autopulse\PasswordController@update')->name('password.handler.update');
+            if (config('autopulse.username_handler', false)) {
+                Route::get('/account/username', 'Autopulse\UsernameController@show')->name('username.handler');
+                Route::post('/account/username', 'Autopulse\UsernameController@update')->name('username.handler.update');
+            }
+        )};
+        if (config('autopulse.cp', false)) {
+            Route::prefix('/cp')->middleware(['password.confirm'])->group(function () {
+                if (config('autopulse.roles', false)) {
+                    Route::resource('/roles', 'Autopulse\RoleController');
+                }
+                if (config('autopulse.permissions', false);
+                    Route::resource('/permissions', 'Autopulse\PermissionController');
+                }
+            )};
         }
     }
 }
