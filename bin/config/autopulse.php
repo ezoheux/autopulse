927fddb951f2c2dcf319e6f61344cfa26b652714
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:32:05 -0500

    Add password validation logic config variable.

--- bin/config/autopulse.php
+++ bin/config/autopulse.php
@@ -3,7 +3,7 @@
 return [
 
     'username_handler' => false,
-
+    'password_validation_logic' => ['required', 'string', 'min:8', 'confirmed'],
     'username_validation_logic' => ['required', 'string', 'alpha_num',  'min:4', 'max:20', 'unique:users', 'confirmed'],
 
 ];
