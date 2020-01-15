6bf5b7e1beb2a7274e9555396857a36a35e094b3
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:44:00 -0500

    Add more.

--- bin/config/autopulse.php
+++ bin/config/autopulse.php
@@ -6,6 +6,7 @@ return [
     'password_validation_logic' => ['required', 'string', 'min:8', 'confirmed'],
     'username_validation_logic' => ['required', 'string', 'alpha_num',  'min:4', 'max:20', 'unique:users', 'confirmed'],
     'cp' => false,
+    'cp_accessor_role' => 'admin',
     'roles' => false,
     'permissions' => false,
 
