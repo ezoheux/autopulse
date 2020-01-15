4edd0de56038c2f00262758d96c6ffc86b0ef817
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:17:55 -0500

    Add username routes.

--- src/Autopulse.php
+++ src/Autopulse.php
@@ -4,6 +4,10 @@ class Autopulse
 {
     public static function routes()
     {
-
+        
+        if (config('username.handler', false)) {
+            Route::get('/account/username', 'Autopulse\UsernameController@show')->name('username.handler');
+            Route::post('/account/username', 'Autopulse\UsernameController@update')->name('username.handler.update');
+        }
     }
 }
