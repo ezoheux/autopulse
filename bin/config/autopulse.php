b605dc07710773ecaee5a1ee6fcbe1eb07d568e8
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:21:26 -0500

    Add validation logic config variable.

--- bin/config/autopulse.php
+++ bin/config/autopulse.php
@@ -2,6 +2,8 @@
 
 return [
 
-    'username.handler' => false,
+    'username_handler' => false,
+
+    'username_validation_logic' => ['required', 'string', 'alpha_num',  'min:4', 'max:20', 'unique:users', 'confirmed'],
 
 ];
