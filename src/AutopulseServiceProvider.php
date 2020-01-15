27e2c7096f2e7fde357eac4b1268d32ff01a7a14
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:29:15 -0500

    Load and publish translations.

--- src/AutopulseServiceProvider.php
+++ src/AutopulseServiceProvider.php
@@ -17,8 +17,10 @@ class AutopulseServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../bin/migrations');
+        $this->loadTranslationsFrom(__DIR__.'/path/to/translations', 'autopulse');
         $this->publishes([
             __DIR__ . '/../bin/config/autopulse.php' => config_path('autopulse.php'),
+            __DIR__ . '/../bin/translations' => resource_path('lang/vendor/courier'),
             __DIR__ . '/../bin/controllers' => app_path('Http/Controllers'),
             __DIR__ . '/../bin/models' => app_path(),
         ]);
