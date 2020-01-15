0fc4ac3496db9d6d4a0d6958f97fbd5f83c4932c
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 02:47:16 -0500

    Add logic.

--- src/Traits/ConnectsToPermissionsModel.php
+++ src/Traits/ConnectsToPermissionsModel.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace Ezoheux\App\Traits;
+
+/**
+ * The connects to permissions model trait.
+ */
+trait ConnectsToPermissionsModel
+{
+    /**
+     * The relationship between the permission model.
+     *
+     * @return mixed Returns the relationship.
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany('App\Permission');
+    }
+}
