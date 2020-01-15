b6f7608c831ca196b6de0e004a620c62b352221e
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-15 03:23:38 -0500

    Add password handler routes.

--- src/Autopulse.php
+++ src/Autopulse.php
@@ -4,7 +4,8 @@ class Autopulse
 {
     public static function routes()
     {
-        
+        Route::get('/account/password', 'Autopulse\PasswordController@show')->name('password.handler');
+        Route::post('/account/password', 'Autopulse\PasswordController@update')->name('password.handler.update');
         if (config('autopulse.username_handler', false)) {
             Route::get('/account/username', 'Autopulse\UsernameController@show')->name('username.handler');
             Route::post('/account/username', 'Autopulse\UsernameController@update')->name('username.handler.update');
