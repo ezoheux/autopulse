034d67cad58b350482774f771153e05376a5d2e0
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-26 21:11:56 -0500

    Add role mismatch config variable.

--- bin/config/autopulse.php
+++ bin/config/autopulse.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'role_mismatch_redirect' => '/home',
+    
     'username_handler' => false,
     'password_validation_logic' => ['required', 'string', 'min:8', 'confirmed'],
     'username_validation_logic' => ['required', 'string', 'alpha_num',  'min:4', 'max:20', 'unique:users', 'confirmed'],
