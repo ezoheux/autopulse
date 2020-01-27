be22d91cd797c37dc709522442c0041d244b2158
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-26 21:37:16 -0500

    Add permission logic.

--- src/Http/Middleware/PermissionCheck.php
+++ src/Http/Middleware/PermissionCheck.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Ezoheux\App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * The permission check middleware.
+ */
+class PermissionCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request The incoming HTTP request.
+     * @param \Closure                 $next    The next middleware.
+     *
+     * @return mixed Returns the response.
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+        if (!Auth::user()->hasPermission($permission)) {
+            return redirect()->route(config('autopulse.permission_mismatch_redirect'));
+        }
+        return $next($request);
+    }
+}
