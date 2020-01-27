54d78b83bf9c3fdc59762b63d7002fbf5e836d57
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-26 21:21:33 -0500

    Add impersonation check middleware.

--- src/Http/Middleware/ImpersonationCheck.php
+++ src/Http/Middleware/ImpersonationCheck.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Ezoheux\App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * The impersonation check middleware.
+ */
+class ImpersonationCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request The incoming HTTP request.
+     * @param \Closure                 $next    The next middleware.
+     *
+     * @return mixed Returns the response.
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (session()->has('impersonating')) {
+            Auth::onceUsingId(session()->get('impersonating'));
+        }
+        return $next($request);
+    }
+}
